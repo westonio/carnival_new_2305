27015beb1f63193eb7e3fdb5b70f635de4682148
Weston Schutt <wtschutt@gmail.com>
2023-06-13 11:06:38 -0600

    Feature: Add .summary method and helpers

--- lib/carnival.rb
+++ lib/carnival.rb
@@ -22,4 +22,52 @@ class Carnival
   def total_revenue
     rides.sum { |ride| ride.total_revenue}
   end
+
+  def summary
+    {
+      visitor_count: visitor_count,
+      revenue_earned: total_revenue,
+      visitors: visitors_summary,
+      rides: rides_summary
+    }
+  end
+
+  #helper for .visitor_count and .visitors_summary
+  def unique_visitors
+    rides.flat_map do |ride|
+      ride.rider_log.keys
+    end.uniq
+  end
+
+  #helper for .summary
+  def visitor_count
+    unique_visitors.count
+  end
+
+  #helper for .visitors_summary
+  def favorite_ride(visitor)
+    rides.max_by { |ride| ride.rider_log[visitor] }
+  end
+  
+  #helper for .summary
+  def visitors_summary
+    unique_visitors.each do |visitor|
+      {
+        visitor: visitor,
+        favorite_ride: favorite_ride(visitor),
+        total_money_spent: visitor.money_spent
+      }
+    end
+  end
+
+  #helper for .summary
+  def rides_summary
+    rides.each do |ride|
+      {
+        ride: ride,
+        riders: ride.rider_log.keys,
+        total_revenue: ride.total_revenue
+      }
+    end
+  end
 end
